b504bb5473aeaaf2c7d436353c908a8472a1225d
Shreyash2098 <143548443+Shreyash2098@users.noreply.github.com>
2023-11-05 00:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,27 @@
-// Your Script here.
+function rot13(str) {
+  // Define a helper function to decode a single character
+  function decodeChar(char) {
+    const charCode = char.charCodeAt(0);
 
-const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
+    if (char >= 'A' && char <= 'Z') {
+      // Decode uppercase letters
+      return String.fromCharCode(((charCode - 'A'.charCodeAt(0) + 13) % 26) + 'A'.charCodeAt(0));
+    } else if (char >= 'a' && char <= 'z') {
+      // Decode lowercase letters
+      return String.fromCharCode(((charCode - 'a'.charCodeAt(0) + 13) % 26) + 'a'.charCodeAt(0));
+    } else {
+      // Pass non-alphabetic characters unchanged
+      return char;
+    }
+  }
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-
-  return; //return decodedArr
+  // Use the decodeChar function to decode the entire string
+  const decodedStr = str.split('').map(decodeChar).join('');
+  return decodedStr;
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+// Example usage:
+const encodedString = 'SERR PBQR PNZC';
+const decodedString = rot13(encodedString);
+console.log(decodedString); // Output: 'FREE CODE CAMP'
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
